8f59ea6fa143ec1cce4956d2505feef70b2d7e92
derickfelix <derickfelix4321@gmail.com>
2017-12-09 19:54:33 -0500

    add GameObject

--- GameObject.java
+++ GameObject.java
@@ -12,4 +12,24 @@ public abstract class GameObject {
 	public abstract void update();
 	public abstract void draw(Graphics2D g);
 	public abstract Rectangle getBounds();
+	
+	public ID getID() {
+		return id;
+	}
+	
+	public int getX() { 
+		return x; 
+	}
+	
+	public void setX(int x) { 
+		this.x = x;
+	}
+	
+	public int getY() { 
+		return y; 
+	}
+	
+	public void setY(int y) {
+		this.y = y; 
+	}
 }
